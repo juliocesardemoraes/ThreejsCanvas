4d939101867cac60709fae752421ca79cf9d818a
julio cesar <jcmcf2012@gmail.com>
2022-12-23 12:38:43 -0300

    fix: new html structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import CanvasThreeJs from "./components/Canvas/Canvas";
 function App() {
   return (
     <div className="App">
-      <h1 className="title">THREEJS PROJECTS</h1>
-      <CanvasThreeJs></CanvasThreeJs>
-      <div className="cards">
-        <div>
-          <img />
-          <h4>Amazing 3d text render with threejs</h4>
-          <label>3D Text</label>
+      <div className="main__container">
+        <h1 className="title">THREEJS PROJECTS</h1>
+        <CanvasThreeJs></CanvasThreeJs>
+        <div className="main__container__cards">
+          <div>
+            <img />
+            <h4>Amazing 3d text render with threejs</h4>
+            <label>3D Text</label>
+          </div>
+          <div>2</div>
+          <div>3</div>
         </div>
-        <div>2</div>
-        <div>3</div>
       </div>
     </div>
   );
