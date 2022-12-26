b08823e3b96f5610f960222df6846b0cadc9a8f4
julio cesar <jcmcf2012@gmail.com>
2022-12-26 17:28:02 -0300

    fix: change logic, and remove unused variables

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import "./index.css";
 import * as THREE from "three";
 import getThreeProject from "../Projects/threejsprojects/getThreeProject";
@@ -12,47 +11,33 @@ interface IButtonParams {
   scene: THREE.Scene;
 }
 
+type ProjectName = "threeJsText" | "threeJsGame";
+
 const removeFromRender = (
-  checkIfRendered: boolean,
-  setCheckIfRendered: React.Dispatch<React.SetStateAction<boolean>>,
   scene: THREE.Scene,
-  projectName: string,
+  projectName: ProjectName,
   setAnyRendered: React.Dispatch<React.SetStateAction<boolean>>,
   isAnyRendered: boolean
 ) => {
-  if (checkIfRendered === false) {
-    if (isAnyRendered === true) {
-      scene.children = [];
-    }
-    getThreeProject(scene, projectName);
-    setCheckIfRendered(true);
-    setAnyRendered(true);
-  } else {
+  if (isAnyRendered === true) {
     scene.children = [];
-    setCheckIfRendered(false);
-    setAnyRendered(false);
+  } else {
+    setAnyRendered(true);
   }
+  getThreeProject(scene, projectName);
 };
 
-const checkIfAnyRendered = atom("false");
+const checkIfAnyRendered = atom(false);
 
 export default function Button(props: IButtonParams) {
   const { img, imgTitle, title, label, scene } = props;
   const [isAnyRendered, setAnyRendered] = useAtom(checkIfAnyRendered);
-  const [checkIfRendered, setCheckIfRendered] = useState(false);
 
   return (
     <div
       className="buttonContainer"
       onClick={() => {
-        removeFromRender(
-          checkIfRendered,
-          setCheckIfRendered,
-          scene,
-          "threeJsText",
-          setAnyRendered,
-          isAnyRendered
-        );
+        removeFromRender(scene, "threeJsText", setAnyRendered, isAnyRendered);
       }}
     >
       <img src={img} alt={imgTitle} />
